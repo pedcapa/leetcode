cb5092648cfa78904ee83960dd10f30e4fe48114
Pedro Castañeda <catbox.py@gmail.com>
2024-08-19 16:40:34 -0600

    650. 2 Keys Keyboard | 3ms beats 69.44%

---
+++ 650-2KeysKeyboard.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int miniSteps(int n);
+
+int miniSteps(int n)
+{
+  // caso base
+  if (n == 1) return 0;
+
+  // par e impar
+  int tmp_n = 1, steps = 0;
+
+  for (int i = 2; i < 1000; ++i)
+  {
+    if (n % i == 0)
+    {
+      steps += i;
+      n /= i;
+      --i;
+    }
+    if (n == 0) break;
+  }
+  return steps;
+}
+
+int main()
+{
+  int n;
+  std::cin >> n;
+
+  std::cout << miniSteps(n) << std::endl;
+
+  return 0;
+}
