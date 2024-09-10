e4477c2fa527f756a6c1d7ace1a7663fff4f1861
Pedro Castañeda <catbox.py@gmail.com>
2024-09-09 20:26:02 -0600

    2807. Insert Greatest Common Divisors in Linked List | 27 ms beats 97.17%

---
+++ 2807-InsertGreatestCommonDivisorsInLinkedList_tmp.cpp
@@ -0,0 +1,39 @@
+static const int speedup = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* node1 = head;
+        ListNode* node2 = head->next;
+
+        while (node2 != nullptr) {
+            int gcdValue = calculateGCD(node1->val, node2->val);
+            ListNode* gcdNode = new ListNode(gcdValue);
+
+            node1->next = gcdNode;
+            gcdNode->next = node2;
+
+            node1 = node2;
+            node2 = node2->next;
+        }
+
+        return head;
+    }
+
+private:
+    int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
