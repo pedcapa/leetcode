651a30c023204b8e9f762e1de18d6305da7000f6
Pedro Castañeda <catbox.py@gmail.com>
2024-09-17 22:25:47 -0600

    179. Largest Number | 3 ms beats 93.74%

---
+++ 179-LargestNumber.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
+        
+        vector<string> array;
+        for (int num : nums) {
+            array.push_back(to_string(num));
+        }
+
+        sort(array.begin(), array.end(), [](const string &a, const string &b) {
+            return (b + a) < (a + b);
+        });
+
+        for(int i = 0; i< array.size() ; i++)cout<<array[i]<<" "; 
+
+        if (array[0] == "0") {
+            return "0";
+        }
+
+        string largest;
+        for (const string &num : array) {
+            largest += num;
+        }
+
+        return largest;
+    }
+};
